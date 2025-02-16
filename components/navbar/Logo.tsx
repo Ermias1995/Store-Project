a35c610fc799e9c56db58ddd96a853bd30156eea
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2025-02-16 14:53:02 +0300

    make cart button and Logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,6 +1,15 @@
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import { IoMdAppstore } from "react-icons/io";
+
+
 function Logo() {
   return (
-    <div>Logo</div>
+    <Button size='icon' asChild>
+      <Link href="/">
+        <IoMdAppstore className="w-6 h-6"/>
+      </Link>
+    </Button>
   )
 }
 export default Logo
