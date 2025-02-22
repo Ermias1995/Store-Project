e557dc407033654e147440b9fefeb1daaff1a6ae
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2025-02-22 17:26:44 +0300

    Add Hero section

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,6 +1,23 @@
+import HeroCarousel from "./HeroCarousel"
+import {Button} from "../ui/button";
+import Link from "next/link";
+
 function Hero() {
   return (
-    <div>Hero</div>
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl">
+          We are changing the way people shop
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <Button asChild size='lg' className="mt-10">
+          <Link href='/products'>Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel/>
+    </section>
   )
 }
 export default Hero
